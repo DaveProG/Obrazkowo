b2de34762682640f527e08efc0e0f22168a04c13
DaveProG <dawid.gruchala@o2.pl>
2020-06-28 13:02:34 +0200

    powiązanie pinów z użytkownikami i uwierzytelnienie

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_101929) do
+ActiveRecord::Schema.define(version: 2020_06_28_100857) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
